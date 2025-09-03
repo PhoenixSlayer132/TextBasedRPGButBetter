677b78f43d2a52d2fb359f7d4c79acdd9c5fc666
Adarsh Parth Kapur <adarshparthkapur132@gmail.com>
2025-09-03 13:46:09 -0400

    Monster Types and Elements Implemented

--- TextBasedRPG/BackEnd/Monsters/MonsterType.cs
+++ TextBasedRPG/BackEnd/Monsters/MonsterType.cs
@@ -1,7 +1,74 @@
-﻿namespace TextBasedRPG.BackEnd.Monsters;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.IO;
+
+namespace TextBasedRPG.BackEnd.Monsters;
 
 public class MonsterType {
+    
+    public static MonsterType randomizedTypes() {
+        Random random = new Random();
+        var types = Enum.GetNames(typeof(MonsterType));
+        int randomTypeIndex = random.Next(0, types.Length);
+        eMonsterType randomType = (eMonsterType)randomTypeIndex;
 
+        eElement randomElement;
+        switch (randomType)
+        {
+            case 0: {//Humanoid
+                string[] elements = ["Fire",  "Water", "Earth", "Wind", "Null"];
+                int randomElementIndex = random.Next(0, elements.Length);
+                randomElement = (eElement)randomElementIndex;
+                break;
+            }
+            case (eMonsterType)1: {//Feral
+                string[] elements = ["Fire",  "Water", "Earth", "Wind", "Null"];
+                int randomElementIndex = random.Next(0, elements.Length);
+                while (randomElementIndex == 0) 
+                    randomElementIndex = random.Next(0, elements.Length);
+                randomElement = (eElement)randomElementIndex;
+                break;
+            }
+            case (eMonsterType)2: {//Elemental
+                string[] elements = ["Fire",  "Water", "Earth", "Wind", "Null"];
+                int randomElementIndex = random.Next(0, elements.Length);
+                while (randomElementIndex == 4)
+                    randomElementIndex = random.Next(0, elements.Length);
+                randomElement = (eElement)randomElementIndex;
+                break;
+            }
+            case (eMonsterType)3: {//Flora
+                string[] elements = ["Fire",  "Water", "Earth", "Wind", "Null"];
+                int randomElementIndex = random.Next(0, elements.Length);
+                while (randomElementIndex == 0 || randomElementIndex == 3 || randomElementIndex == 4)
+                    randomElementIndex = random.Next(0, elements.Length);
+                randomElement = (eElement)randomElementIndex;
+                break;
+            }
+            case (eMonsterType)4: {//Undead
+                string[] elements = ["Fire",  "Water", "Earth", "Wind", "Null"];
+                int randomElementIndex = random.Next(0, elements.Length);
+                while (randomElementIndex == 1)
+                    randomElementIndex = random.Next(0, elements.Length);
+                randomElement = (eElement)randomElementIndex;
+                break;
+            }
+            case (eMonsterType)5: {//Draconic
+                string[] elements = ["Fire",  "Water", "Earth", "Wind", "Null"];
+                int randomElementIndex = random.Next(0, elements.Length);
+                while (randomElementIndex == 4)
+                    randomElementIndex = random.Next(0, elements.Length);
+                randomElement = (eElement)randomElementIndex;
+                break;
+            }
+            default:
+                randomElement = (eElement)4;
+                break;
+        }
+
+        MonsterType randMonster = new MonsterType((eMonsterType)randomTypeIndex, randomElement);
+        return randMonster;
+    }
+    
     public enum eMonsterType {
         Humanoid,
         Feral,
@@ -11,24 +78,18 @@ public class MonsterType {
         Draconic,
         Abomination
     }
-    public MonsterType() {
+    public MonsterType(eMonsterType type, eElement element) {
         
     }
-    public class MonsterElement {
-        public enum eElement {
-            Fire,
-            Water,
-            Earth,
-            Wind,
-            Null
-        }
-    
-        public MonsterElement(eElement element) {
-        
-        }
+    public enum eElement {
+        Fire,
+        Water,
+        Earth,
+        Wind,
+        Null
+    }
 
     
-    }
 
     
 } 
--- TextBasedRPG/MainWindow.xaml.cs
+++ TextBasedRPG/MainWindow.xaml.cs
@@ -9,7 +9,6 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using TextBasedRPG.BackEnd.Monsters;
-using TextBasedRPG.BackEnd.Monsters.MonsterTypes;
 using TextBasedRPG.BackEnd.Player;
 
 namespace TextBasedRPG;
@@ -24,11 +23,9 @@ public partial class MainWindow : Window
         Player player = new Player("Phoenix", Player.eProfession.Mage, 0, 
             new PlayerStats(20, 5, 5, 5, 5, 5, 3));
         
-        // Monsters monster = new Monsters("Wolf", 0,
-        //     new MonsterStats(MonsterStats.eSkillLevel.Tutorial, 20, 5, 5, 5, 5, 5, 3),
-        //         new MonsterType(MonsterType.eMonsterType.Wolf,
-        //             new BodyType(BodyType.eBodyType.Feral),
-        //             new MonsterElement(MonsterElement.eElement.Null)));
+        Monsters monster = new Monsters("Wolf", 0,
+            new MonsterStats(MonsterStats.eSkillLevel.Tutorial, 20, 5, 5, 5, 5, 5, 3), 
+            MonsterType.randomizedTypes());
         
         InitializeComponent();
     }
